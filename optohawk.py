d05d104908a712f1072995d45d6d32b7322aa65a
Pratyay Banerjee <48355572+Neilblaze@users.noreply.github.com>
2021-02-21 13:35:25 +0530

    populate cv2 to vid

--- optohawk.py
+++ optohawk.py
@@ -6,10 +6,24 @@
 # In[1]:
 
 import os
+import cv2
 import argparse
-
 
 parser = argparse.ArgumentParser()
 parser.add_argument("ROOT_VID", help="Path to the video to be summarized")
 parser.add_argument("--INT_BW_DIV", help="Split moving objects - root.this => larger video => low overlapping")
 args = parser.parse_args()
+
+# In[2]:
+
+ROOT_VID = args.ROOT_VID
+
+cap  = cv2.VideoCapture(ROOT_VID)
+
+MIN_SECONDS =  6
+
+INT_BW_DIV = 15 #to reduce overlapping.
+GAP_BW_DIV = 0.15
+
+if args.INT_BW_DIV:
+    INT_BW_DIV = args.INT_BW_DIV
